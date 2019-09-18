b09c0528796113b179e414c6d44cb6d531621659
jaewon613 <jaewon613.lee@samsung.com>
2019-09-18 16:33:46 +0900

    공통 sort 분리

--- project2/shopping.h
+++ project2/shopping.h
@@ -13,6 +13,12 @@
 //     tobuylist.PushList(obj1);
 //     tobuylist.PushList(obj2);
 //     tobuylist.PrintList();
+enum Type
+{
+	TYPE_PRICE,
+	TYPE_INDEX,
+};
+
 class Shopping{
  public:
   void PushList(Goods object);
@@ -23,6 +29,7 @@ class Shopping{
 
  private:
   std::vector<Goods> shopping_list_;
+  void SortCore(int type);
 };
 
 #endif  // SHOPPING_H_
